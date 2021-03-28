2b5fa54582ce9596f26025d824a62e077a79a0fe
MohamedMF2 <furious_fouad@yahoo.com>
2021-03-28 21:19:33 +0200

    methods to convert into and from a map

--- lib/src/models/itemmodel.dart
+++ lib/src/models/itemmodel.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class ItemModel {
   final int id;
   final bool deleted;
@@ -27,4 +29,37 @@ class ItemModel {
         score = pasrsedJson['score'],
         title = pasrsedJson['title'],
         descendants = pasrsedJson['descendants'];
+
+  ItemModel.fromDb(Map<String, dynamic> pasrsedJson)
+      : id = pasrsedJson['id'],
+        deleted = pasrsedJson['deleted'] == 1,
+        type = pasrsedJson['type'],
+        by = pasrsedJson['by'],
+        time = pasrsedJson['time'],
+        text = pasrsedJson['text'],
+        dead = pasrsedJson['dead'] == 1,
+        parent = pasrsedJson['parent'],
+        kids = json.decode(pasrsedJson['kids']),
+        url = pasrsedJson['url'],
+        score = pasrsedJson['score'],
+        title = pasrsedJson['title'],
+        descendants = pasrsedJson['descendants'];
+
+  Map<String, dynamic> toMapForDb() {
+    return <String, dynamic>{
+      "id": id,
+      "deleted": deleted ? 1 : 0,
+      "type": type,
+      "by": by,
+      "time": time,
+      "text": text,
+      "dead": dead ? 1 : 0,
+      "parent": parent,
+      "url": url,
+      "score": score,
+      "title": title,
+      "descendants": descendants,
+      "kid": jsonEncode(kids)
+    };
+  }
 }
